64c13cfcb63fb21b33ba9c12433b70eb9103a36e
murilo-fernandes <murilofernandeszl.23@gmail.com>
2025-05-28 18:58:30 -0300

    Tela de cadastro criada

--- app/src/main/java/com/example/maissade/Cadastro.java
+++ app/src/main/java/com/example/maissade/Cadastro.java
@@ -1,8 +1,6 @@
 package com.example.maissade;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Login extends AppCompatActivity {
-
-    private Button btnCadastrar;
+public class Cadastro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_cadastro);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        btnCadastrar = findViewById(R.id.btnCadastrar);
-
-        btnCadastrar.setOnClickListener(v -> {
-            Intent intent = new Intent(Login.this, Cadastro.class);
-            startActivity(intent);
-        });
-
-}}
+    }
+}
