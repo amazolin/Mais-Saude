fd21ff4669cb7a8efd9d13a9cb08d4c680dd94e0
murilo-fernandes <murilofernandeszl.23@gmail.com>
2025-06-03 17:47:30 -0300

    IMC atualizado + exclusão botão do google

--- app/src/main/java/com/example/maissade/TelaUsuario.java
+++ app/src/main/java/com/example/maissade/TelaUsuario.java
@@ -363,10 +363,19 @@ public class TelaUsuario extends AppCompatActivity {
             txtIMCPerfil.setText(String.format("%.2f", imc));
 
             String nivelStatus;
-            if (imc < 18.5) nivelStatus = "Abaixo do peso";
-            else if (imc < 24.9) nivelStatus = "Peso normal";
-            else if (imc < 29.9) nivelStatus = "Acima do peso";
-            else nivelStatus = "Obesidade";
+            if (imc < 18.5) {
+                nivelStatus = "Abaixo do peso";
+            } else if (imc < 24.9) {
+                nivelStatus = "Peso normal";
+            } else if (imc < 29.9) {
+                nivelStatus = "Sobrepeso"; // (Acima do peso - Grau I)
+            } else if (imc < 34.9) {
+                nivelStatus = "Obesidade Grau I";
+            } else if (imc < 39.9) {
+                nivelStatus = "Obesidade Grau II (severa)";
+            } else {
+                nivelStatus = "Obesidade Grau III (mórbida)";
+            }
             txtNivelGeralStatus.setText(nivelStatus);
         } else {
             txtIMCPerfil.setText("--");
